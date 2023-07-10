b30c18332179e11e7c4c219cbfd2f552c05cb192
Max <mlmason11@gmail.com>
2023-07-10 15:42:54 -0400

    Submitting scope lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+//bestCustomer = 'not bob';
+const leastFavoriteCustomer = 'jim';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(customerName);
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe not jim';
+}
